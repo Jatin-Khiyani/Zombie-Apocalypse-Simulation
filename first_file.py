ba39b3eecf5c9e9083288c1b8c9987b9ac808771
Jatin-Khiyani <jatinkhiyani14@gmail.com>
2026-03-17 15:20:03 +0100

    Added functions Movement and infection to the class Person. Also added a frame rate counter

--- first_file.py
+++ first_file.py
@@ -1,7 +1,11 @@
+# Code Written using Youtube Tutorial https://youtube.com/playlist?list=PLBLV84VG7Md_JGMCHNOqvAvsTPc7zi1xB&si=FpVEBl7iPVz-pjh5
+
 # Module used to visulize simulation 
+
 import pygame
 import random
-
+import math
+pygame.init()
 # Setting Fixed Screen Size : In the Future could take input from the user
 WIDTH = 1280
 HEIGHT = 720
@@ -12,16 +16,17 @@ COLOR_DEFINATION = {
     "grey" : (35,35,30),
     "light_gray" : (70,70,90),
     "white" : (255,248,240),
-    "red" :(239,71,111),
+    "red" : (239,71,111),
     "blue": (17,138,178),
     "black" : (0,0,0)
 }
 
-# Associating color with different objects 
+# Associating color with different states
 COLORS = {
     "background" : COLOR_DEFINATION["grey"],
     "light_grey" : COLOR_DEFINATION["white"],
-    "healthy" : COLOR_DEFINATION["red"],
+    "healthy" : COLOR_DEFINATION["white"],
+    "infected" : COLOR_DEFINATION["red"],
     "immune" : COLOR_DEFINATION["blue"],
     "dead" : COLOR_DEFINATION["black"]
 
@@ -32,15 +37,53 @@ class Person:
 
     # Function for setting position of a person
     def __init__(self):
+        # Defining Position 
         self.x = random.uniform(0,WIDTH)
         self.y = random.uniform(0,HEIGHT)
-    
+        # Defining Velocity Vector 
+        self.dx = 0
+        self.dy = 0
+        self.state = "healthy"
+
+    # Function to show dots as persons 
     def show(self,size = 10):
-        pygame.draw.circle(SCREEN,COLOR_DEFINATION["white"],(self.x,self.y),size)
+        pygame.draw.circle(SCREEN,COLORS[self.state],(self.x,self.y),size)
 
+    # Function to make dots move in random direction 
+    def move(self,speed = 0.1):
 
-person1 = Person()
-person2 = Person()
+        # Chaning the position according to velocity
+        self.x += self.dx 
+        self.y += self.dy
+
+        # Updating Velocity 
+        self.dx += random.uniform(-speed,speed)
+        self.dy += random.uniform(-speed,speed)
+
+        # Avoid Going out of bounds 
+        if self.x >= WIDTH:
+            self.x = WIDTH - 1
+            self.dx *= -1
+        if self.y  >= HEIGHT:
+            self.y = HEIGHT - 1
+            self.dy *= -1
+        if self.x <=0:
+            self.x = 1
+            self.dx *= -1
+        if self.y <= 0:
+            self.y = 1
+            self.dy *= -1
+    def get_infected(self):
+        self.state = "infected"
+
+# Generating People
+people = [Person() for i in range(1000)]
+people[0].get_infected()
+
+# Code to keep track of frame rate 
+
+clock = pygame.time.Clock()
+font = pygame.font.Font("freesansbold.ttf",32)
 # Used to open and close the window for simulation 
 animating = True
 while animating:
@@ -48,10 +91,26 @@ while animating:
     #Setting Background Color
     SCREEN.fill(COLORS["background"])
 
-    person1.show()
-    person2.show()
- 
-    #Update Screen
+    # Showing people 
+    for p in people:
+        p.move()
+        p.show()
+
+    # for loop  to infect other people (Highly Computationally intesive needed to be changed in the future)
+    for p in people:
+        if p.state == "infected":
+            for other in people:
+                if other.state == "healthy":
+                    dist = math.sqrt((p.x-other.x)**2 + (p.y-other.y)**2)
+                    if dist < 20:
+                        other.get_infected()
+    
+    #Update Screen and the clock
+    clock.tick()
+    clock_string = str(math.floor(clock.get_fps()))
+    text = font.render(clock_string,True,COLOR_DEFINATION["blue"],COLORS["background"])
+    text_box = text.get_rect(topleft=(10,10))
+    SCREEN.blit(text,text_box)
     pygame.display.flip()
 
     for event in pygame.event.get():
